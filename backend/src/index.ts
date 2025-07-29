9d1bcba6dd7987b45304f8f37f101296db0c2c06
Takura <takutadi1@gmail.com>
2025-07-29 22:01:09 +0200

    Added vercel url to allowed URLs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -32,6 +32,7 @@ const corsOptions = {
       'http://localhost:5173', 
       'http://localhost:4173',
       'http://localhost:3001',
+      'https://chow-gpt-qz8o.vercel.app/',
       // Production origins
       'https://chowgpt.onrender.com',
       'https://chowgpt.vercel.app',
